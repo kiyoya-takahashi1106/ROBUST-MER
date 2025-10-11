2bbc941782a2dc1d1a5bf41a81fa68e1c30a2047
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-10-11 09:30:10 +0900

    mp3 => wav file

--- utils/pretrain_dataset.py
+++ utils/pretrain_dataset.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from transformers import AutoFeatureExtractor, VideoMAEImageProcessor
 from decord import VideoReader
 import torchaudio
-# torchaudio.set_audio_backend("soundfile")
+torchaudio.set_audio_backend("soundfile")
 
 import random
 import pandas as pd
@@ -158,12 +158,12 @@ def make_data_combination(sentence_emotion_group_dct):
 
 # 指定されたファイルの各データに対して前処理を行いテンソルで返す
 def pre_process(filename, input_modality, processor):
-    parent_dir = "AudioMP3" if input_modality == "audio" else "VideoFlash"
+    parent_dir = "AudioWAV" if input_modality == "audio" else "VideoFlash"
     file_path = f"data/CREMA-D/raw/{parent_dir}/{filename}"
     
     # モダリティ別処理
     if (input_modality == "audio"):
-        file_path += ".mp3"
+        file_path += ".wav"
         waveform, sr = torchaudio.load(file_path)
         if waveform.size(0) > 1:
             waveform = waveform.mean(dim=0, keepdim=True)
