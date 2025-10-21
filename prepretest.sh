ef3227c2360c8784b34931d6a9095d07725b3b5e
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-10-22 08:29:18 +0900

    change setting

--- prepretest.sh
+++ prepretest.sh
@@ -1,9 +1,9 @@
 SEED=42
 BATCH_SIZE=100
 DATASET_NAME="MOSI"
-CLASS_NUM=6
-INPUT_MODALITY="audio"
-TRAINED_MODEL_FILE=".pth"   # prepretrainで学習した重みファイル名
+CLASS_NUM=2
+INPUT_MODALITY="video"
+TRAINED_MODEL_FILE="MOSI_classNum2_20251022_075740_epoch0_0.6026_seed42_dropout0.3.pth"   # prepretrainで学習した重みファイル名
 HIDDEN_DIM=768
 
 
