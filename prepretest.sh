ebaa0f60dffb9d5d27e68d76a50a9d6085471c1e
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-10-21 14:47:15 +0900

    change sertting

--- prepretest.sh
+++ prepretest.sh
@@ -2,8 +2,8 @@ SEED=42
 BATCH_SIZE=100
 DATASET_NAME="CREMA-D"
 CLASS_NUM=6
-INPUT_MODALITY="audio"
-TRAINED_MODEL_FILE="CREMA-D_classNum6_20251020_224136_seed42_dropout0.3.pth"   # prepretrainで学習した重みファイル名
+INPUT_MODALITY="video"
+TRAINED_MODEL_FILE="CREMA-D_classNum6_20251021_091649_epoch3_0.7660_seed42_dropout0.3.pth"   # prepretrainで学習した重みファイル名
 HIDDEN_DIM=768
 
 LOG_FILE="test_results/prepretest/${INPUT_MODALITY}/${DATASET_NAME}_${CLASS_NUM}_$(date +%Y%m%d_%H%M%S).log"
