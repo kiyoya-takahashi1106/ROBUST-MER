7749914ea6a3ddd3f4e8d15da020dc9b3fa11f55
kiyoya.takahashi <e1922030@oit.ac.jp>
2025-10-21 10:40:29 +0900

    change data split method along CREMA-D{prepre, pre}Dataset.py

--- utils/train_dataset_CREMAD.py
+++ utils/train_dataset_CREMAD.py
@@ -1,5 +1,3 @@
-# pretrain2.py {train, val} と同じ分け方
-
 import torch    
 from torch.utils.data import Dataset
 import torch.nn.functional as F
@@ -12,23 +10,15 @@ torchaudio.set_audio_backend("soundfile")
 import pandas as pd
 import numpy as np
 import random
-
-
-class CREMADDataProvider:
-    def __init__(self, seed):
-        self.seed = seed
-        self.train_sentence_emotion_group_dct, self.val_sentence_emotion_group_dct = cremed_classification(seed)
-        self.train_dataset = transform2prepre(self.train_sentence_emotion_group_dct)
-        self.val_dataset = transform2prepre(self.val_sentence_emotion_group_dct)
-
-    def get_dataset(self):
-        return self.train_dataset, self.val_dataset
-
+import os
 
 
 class CREMADDataset(Dataset):
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, split):
+        self.split = split
+
+        self.data = get_data(self.split)
+
         self.audio_processor = AutoFeatureExtractor.from_pretrained("microsoft/wavlm-base-plus")
         self.video_processor = VideoMAEImageProcessor.from_pretrained("MCG-NJU/videomae-base")
 
@@ -37,90 +27,41 @@ class CREMADDataset(Dataset):
 
     def __getitem__(self, index):
         sample = self.data[index]
-        filename, label = sample
+        filename, label, group_label = sample
         audio_x, audio_mask = pre_process(filename, "audio", self.audio_processor)
         video_x, video_mask = pre_process(filename, "video", self.video_processor)
-        return audio_x, video_x, audio_mask, video_mask, label
+        return audio_x, video_x, audio_mask, video_mask, label, group_label
 
 
 
-# すべてのデータを (テキスト12 × 感情6) × grpoup4 に分類するし、それをtrainとvalに分割
-def cremed_classification(seed):
-    # CSVファイルを読み込み
-    actors_file = pd.read_csv('./data/CREMA-D/raw/VideoDemographics.csv')
-    actors_file_content = actors_file.copy()
-    datas_file = pd.read_csv('./data/CREMA-D/raw/SentenceFilenames.csv')
-    datas_file_content = datas_file.copy()
-
-    # 各actorがgroupのどこに属するかを確認
-    actor_dct = {}
-    age_threshold = 40
-    for _, row in actors_file_content.iterrows():
-        actor_id = row["ActorID"]
-        age = row["Age"]
-        gender = row["Sex"]
-            
-        if age <= age_threshold:
-            if gender == 'Male':  
-                actor_dct[actor_id] = 1
-            else:  
-                actor_dct[actor_id] = 3
-        else: 
-            if gender == 'Male':
-                actor_dct[actor_id] = 2
-            else:
-                actor_dct[actor_id] = 4
-
-    sentence_emotion_group_dct = {}   # テキスト_感情_group -> datafile名のリスト
-    for _, row in datas_file_content.iterrows():
-        filename = row["Filename"]
-        part = filename.split('_')
-        actor_id = int(part[0])
-        sentence_emotion = part[1] + "_" + part[2]
-
-        if sentence_emotion not in sentence_emotion_group_dct:
-            sentence_emotion_group_dct[sentence_emotion] = {1: [], 2: [], 3: [], 4: []}
-        
-        actor_group = actor_dct[actor_id]
-        sentence_emotion_group_dct[sentence_emotion][actor_group].append(filename)
-
-    # train/val分割
-    train_sentence_emotion_group_dct = {}
-    val_sentence_emotion_group_dct = {}
-    rng = random.Random(seed)   # valの組み合わせは毎回同じ
-    
-    for sentence_emotion, group_dct in sentence_emotion_group_dct.items():
-        min_len = min(len(group_dct[1]), len(group_dct[2]), len(group_dct[3]), len(group_dct[4]))
-        train_sentence_emotion_group_dct[sentence_emotion] = {1: [], 2: [], 3: [], 4: []}
-        val_sentence_emotion_group_dct[sentence_emotion] = {1: [], 2: [], 3: [], 4: []}
-
-        for group_num in [1, 2, 3, 4]:
-            filename_lst = group_dct[group_num].copy()
-            rng.shuffle(filename_lst)
-            
-            val_num = int(min_len * 0.2)
-            val_filename_lst = filename_lst[:val_num]
-            val_sentence_emotion_group_dct[sentence_emotion][group_num] = val_filename_lst
-
-            train_filename_lst = filename_lst[val_num:]
-            train_sentence_emotion_group_dct[sentence_emotion][group_num] = train_filename_lst
-
-    return train_sentence_emotion_group_dct, val_sentence_emotion_group_dct
-
-
-
-def transform2prepre(sentence_emotion_group_dct):
-    data = []
+def get_data(split):
+    data_folder_path = f"./data/CREMA-D/{split}/AudioWAV/"   # videoでも拡張子 以外のところは同じファイル名なのでこれでOK
     emotion_label_dct = {"ANG": 0, "DIS": 1, "FEA": 2, "HAP": 3, "NEU": 4, "SAD": 5}
 
-    for sentence_emotion, group_dct in sentence_emotion_group_dct.items():
-        emotion = sentence_emotion.split('_')[1]
-        label = emotion_label_dct[emotion]
-        for group_num in [1, 2, 3, 4]:
-            for i in range(len(group_dct[group_num])):
-                sample = [group_dct[group_num][i]]
-                sample = sample + [label]
-                data.append(sample)
+    # group_label: 0=若い男性, 1=年配男性, 2=若い女性, 3=年配女性
+    group_label_dct = {}   # key: actor_id, value: group_label
+    for _, row in pd.read_csv('./data/CREMA-D/raw/VideoDemographics.csv').iterrows():
+        age_threshold = 40
+        actor_id = int(row["ActorID"])
+        age = row["Age"]
+        gender = row["Sex"]
+        if (age < age_threshold):
+            if (gender == 'Male'):
+                group_label_dct[actor_id] = 0
+            else:
+                group_label_dct[actor_id] = 2
+        else:
+            if (gender == 'Male'):
+                group_label_dct[actor_id] = 1
+            else:
+                group_label_dct[actor_id] = 3
+
+    data = []
+    for file in os.listdir(data_folder_path):
+        file = file.replace(".wav", "")
+        label = emotion_label_dct[file.split("_")[2]]
+        actor_id = int(file.split("_")[0])
+        data.append((file, label, group_label_dct[actor_id]))
 
     return data
 
